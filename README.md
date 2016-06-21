b0aacd9c6181875dd88660361b75e01b5b96d0af
Michael Pace <mpace@myocv.com>
2016-06-20 22:46:05 -0600

    Initialized repo: created base AppCode project with Unit/UI Tests

---
+++ README.md
@@ -0,0 +1,1 @@
+TODO: add some shit to dis readme.
---
+++ iOS Scaffold/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  iOS Scaffold
+//
+//  Created by Michael Pace on 6/20/16.
+//  Copyright (c) 2016 Michael Pace. All rights reserved.
+//
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+}
